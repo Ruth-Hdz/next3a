90879068200bbfdcd4320696247cfde335ad5c07
RuthHdz <ruthhdz11@outlook.es>
2023-07-12 11:38:51 -0600

    message

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,26 @@
+"use client"
+import { useState } from "react";
 import "./login.css"
+
 export default function login(){
+    
+  const [state, setEstate] = useState({
+    email:"",
+    password:""
+});
+
+    const handleChange = (event)=>{
+        const{name,value}= event.target;
+        setEstate({
+            ...state,
+            [name]:value
+        })
+    }
     return (
         <div className="container">
-            <input type="text" placeholder="email"/> 
+            <input onChange={handleChange} name="email" value={state.email} type="text" placeholder="email"/> 
             <input type="password" placeholder="123456"/>
+
             <button>login</button>  
             </div>
     )
